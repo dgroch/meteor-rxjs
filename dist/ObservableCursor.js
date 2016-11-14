d007d1c72a4f264209a2795dff9e40460be5aad2
dgroch <daniel.j.groch@gmail.com>
2016-11-15 09:44:39 +1100

    define __extends inside ObservableCursor

--- dist/ObservableCursor.js
+++ dist/ObservableCursor.js
@@ -2,6 +2,12 @@
 import { Observable, Subject } from 'rxjs';
 import { gZone, forkZone, removeObserver } from './utils';
 export var ObservableCursor = (function (_super) {
+    var __extends = this.__extends || function (d, b) {
+        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+        function __() { this.constructor = d; }
+        __.prototype = b.prototype;
+        d.prototype = new __();
+    };
     __extends(ObservableCursor, _super);
     /**
      * @constructor
